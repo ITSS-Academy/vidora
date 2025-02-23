1e79220e2c7b242e733e959c39051e5fa8b98de9
Nhi <nhi.psh07202@sinhvien.hoasen.edu.vn>
2025-02-23 22:28:01 +0700

    feat: implement playlist and playlist card components with basic structure and styling

--- client/src/app/components/playlist-card/playlist-card.component.ts
+++ client/src/app/components/playlist-card/playlist-card.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-playlist-card',
   standalone: true,
@@ -8,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './playlist-card.component.scss'
 })
 export class PlaylistCardComponent {
+  playlist=
+    {
+      title: 'Playlist 1',
+    image: 'https://i.ytimg.com/vi/a2jNL1Jusi0/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC-YRu9VYrK8L8ayV6jwb80hcwD1g',
+    }
 
 }
