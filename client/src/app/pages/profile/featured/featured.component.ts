92acdc5f0585bd69ea8250f721f87aa26dd71afb
Robert <phuong.bt03177@sinhvien.hoasen.edu.vn>
2025-02-24 12:19:00 +0700

    feat: added video vertical

--- client/src/app/pages/profile/featured/featured.component.ts
+++ client/src/app/pages/profile/featured/featured.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatIconButton} from '@angular/material/button';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader, MatCardMdImage,
+  MatCardSubtitle,
+  MatCardTitle,
+  MatCardTitleGroup
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-featured',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatIconButton,
+    MatCardHeader,
+    MatCard,
+    MatCardTitle,
+    MatCardTitleGroup,
+    MatCardContent,
+    MatCardSubtitle,
+    MatCardMdImage
+  ],
   templateUrl: './featured.component.html',
   styleUrl: './featured.component.scss'
 })
