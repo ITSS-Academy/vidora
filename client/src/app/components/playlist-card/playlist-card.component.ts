7461032ee7f25b3c83cd5be6b08dc43205cab298
Nhi <nhi.psh07202@sinhvien.hoasen.edu.vn>
2025-02-24 12:21:58 +0700

    feat:

--- client/src/app/components/playlist-card/playlist-card.component.ts
+++ client/src/app/components/playlist-card/playlist-card.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
+import {MatButtonModule, MatIconButton} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-playlist-card',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule, MatMenuModule, MatIconModule,CommonModule
+  ],
   templateUrl: './playlist-card.component.html',
   styleUrl: './playlist-card.component.scss'
 })
 export class PlaylistCardComponent {
+  isHovered: boolean = false;
   playlist=
     {
       title: 'Playlist 1',
     image: 'https://i.ytimg.com/vi/a2jNL1Jusi0/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC-YRu9VYrK8L8ayV6jwb80hcwD1g',
     }
+  onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isHovered = false;
+  }
 
 }
