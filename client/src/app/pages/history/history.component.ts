3aaef5725d3e0b3faf6569461503e8401a0c25a4
Quoc Cuong <cuong.hq07381@sinhvien.hoasen.edu.vn>
2025-03-03 09:40:58 +0700

    feat: implement history component layout with video card and action buttons

--- client/src/app/pages/history/history.component.ts
+++ client/src/app/pages/history/history.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/modules/shared.module';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { VideoModule } from '../../../shared/modules/video.module';
+import { VideoCardHorizontalComponent } from '../../components/video-card-horizontal/video-card-horizontal.component';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [SharedModule, MaterialModule, VideoModule],
+  imports: [
+    SharedModule,
+    MaterialModule,
+    VideoModule,
+    VideoCardHorizontalComponent,
+  ],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss',
 })
